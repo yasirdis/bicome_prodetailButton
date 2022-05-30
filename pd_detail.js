027d330594fba29b045f41702cfcaf62d23b3953
yasirdis <yasir.dis@gmail.com>
2022-05-30 13:31:42 +0530

    dss

--- pd_detail.js
+++ pd_detail.js
@@ -2,6 +2,8 @@ window.onload=( function() {
 
 var widget = document.getElementsByClassName("card-figcaption-body");
 
-widget[0].innerHTML = widget[0].innerHTML + '<a class="button button--small card-figcaption-button " data-product-id="103" href={{url}}>Product detail</a>';
+widget[0].innerHTML = widget[0].innerHTML + '<a class="button button--small card-figcaption-button " data-product-id="103" href={{url}}>Product detail</a>'+
+    '{{url}}'+
+    "{{url}}";
 
     });
