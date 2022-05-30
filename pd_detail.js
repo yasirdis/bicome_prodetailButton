42c2804ef7d397e44331de16d77d022f2a80dee1
yasirdis <yasir.dis@gmail.com>
2022-05-30 12:05:32 +0530

    button str added

--- pd_detail.js
+++ pd_detail.js
@@ -1,8 +1,7 @@
 window.onload=( function() {
 
 var widget = document.getElementsByClassName("card-figcaption-body");
-var prod_detail = <a class="button button--small card-figcaption-button quickview" data-product-id="103">Quick view</a>
 
-widget[0].innerHTML = widget[0].innerHTML + '<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Launch demo modal</button>'+model;
+widget[0].innerHTML = widget[0].innerHTML + '<a class="button button--small card-figcaption-button quickview" data-product-id="103">Product detail</a>'+model;
 
     });
