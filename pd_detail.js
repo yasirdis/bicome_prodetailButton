76bae643b92391f8860cbc73e3fd34a48a9db1a1
yasirdis <yasir.dis@gmail.com>
2022-05-30 15:25:04 +0530

    jkhj

--- pd_detail.js
+++ pd_detail.js
@@ -1,12 +1,15 @@
 window.onload=( function() {
 var widget = document.getElementsByClassName("card-figcaption-body");
+for(var i=0;i<widget.length;i++){
+    const el = widget[i].closest('.card-figure');
 
-const el = widget[0].closest('.card-figure');
+var url = el.querySelectorAll('.card-figure__link')[i].href;
 
-var url = el.querySelectorAll('.card-figure__link')[0].href;
+    
 
+widget[i].innerHTML = widget[i].innerHTML + '<a class="button button--small card-figcaption-button " data-product-id="103" href='+url+'>Product detail</a>';
 
-widget[0].innerHTML = widget[0].innerHTML + '<a class="button button--small card-figcaption-button " data-product-id="103" href='+url+'>Product detail</a>';
-
+    
+}
 
     });
