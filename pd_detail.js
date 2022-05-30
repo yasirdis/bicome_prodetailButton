fd1bb44b6a7cff1b244b96e4a5d559cee552503f
yasirdis <yasir.dis@gmail.com>
2022-05-30 11:52:51 +0530

    pdButton

---
+++ pd_detail.js
@@ -0,0 +1,8 @@
+window.onload=( function() {
+
+var widget = document.getElementsByClassName("card-figcaption-body");
+var prod_detail = <a class="button button--small card-figcaption-button quickview" data-product-id="103">Quick view</a>
+
+widget[0].innerHTML = widget[0].innerHTML + '<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Launch demo modal</button>'+model;
+
+    });
