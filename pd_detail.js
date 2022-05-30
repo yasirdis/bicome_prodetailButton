79614d9b09e7108f84030b387c0909111a90b98e
yasirdis <yasir.dis@gmail.com>
2022-05-30 15:18:31 +0530

    awewe

--- pd_detail.js
+++ pd_detail.js
@@ -1,9 +1,12 @@
 window.onload=( function() {
-
 var widget = document.getElementsByClassName("card-figcaption-body");
 
-widget[0].innerHTML = widget[0].innerHTML + '<a class="button button--small card-figcaption-button " data-product-id="103" href={{url}}>Product detail</a>'+
-    '{{url}}'+
-    "{{url}}";
+const el = widget[0].closest('.card-figure');
+
+var url = el.querySelectorAll('.card-figure__link')[0].href;
+
+
+widget[0].innerHTML = widget[0].innerHTML + '<a class="button button--small card-figcaption-button " data-product-id="103" href='+url+'>Product detail</a>';
+
 
     });
