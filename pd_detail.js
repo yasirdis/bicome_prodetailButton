986dbf30b188fe0a04fbc360c526d7bcb531353d
yasirdis <yasir.dis@gmail.com>
2022-06-02 18:13:15 +0530

    dfsdfsf

--- pd_detail.js
+++ pd_detail.js
@@ -12,12 +12,57 @@ widget[i].innerHTML = widget[i].innerHTML + '<a class="button button--small card
 
     
 }
-    if(widget = document.getElementsByClassName("productView")[0]){
+    if(document.getElementsByClassName("productView")[0]){
+        
         var widget = document.getElementsByClassName("productView-options");
-widget[0].innerHTML = widget[0].innerHTML + '<button type="button" class="button button--primary">BOPIS</button>';
         getLatLang();
-        alert("aaaaaaaaaaaaaa");
+widget[0].innerHTML = widget[0].innerHTML+
+        
+        '<input type="hidden" id="postalcodes"  value=""/>'+
+'<input type="hidden" id="stores" value="" />'+
+'<input type="hidden" id="addToCartUrl" value="" />'+
+'<input type="hidden" id="apibaseurl"  value="" />'+
+'<input type="hidden" id="sitebaseurl"  value="" />'+
+'<input type="hidden" id="custIp" name="custIp"  value="" />'+
+'<input type="hidden" id="searchterm" name="searchterm" value="" />'+
+ '   <input type="hidden" id="store" name="store"   value="" />'+
+'<input type="hidden" id="modalopen"   value="openModal" />'+
+'<input type="hidden" id="modalclose"    value="closeModal" />'+
+ '   <div id="quiversmodalappend"  class="quiversaction" >'+
+  '                  <button type="button" '+
+   '                         title="Pick up"'+
+                            'class="action primary quiverspickitup"'+
+    '                        onclick="addtocartpickup('openModal');"'+
+     '                       id="product-pickitup-button" >'+
+      '                  <span>"Pick up"</span>'+
+       '             </button>'+
+        '</div>'+
+'<div id="store-modal" style="display:none;">'+
+    '<div class="quiverswrapper">'+
+ '       <label class="labelzipcode">Showing location near zipcode:</label>'+
+  '      <input type="text" class="zipcode" id="zipcode" value="" >'+
+   '     <button type="button" onclick="changezipcode()"><i class="fa fa-search"></i></button>'+
+      '  <input type="hidden" class="edit" id="edit" value="">'+
+    '</div>'+
+    '<div class="popup-body" id="popup-body">'+
+    '<div class="alert alert-danger fade in alert-dismissible">'+
+    'Could not find stores for this product near your location. Please change the zipcode and try again.'+
+    '</div>'+
+    '</div>'+
+'</div>'
     }
-    
+     require(['jquery',
+    'BopisHosted',
+     'Bopis'], function($) {
+        $('#quivers-config-option').change(function(){
+            getLatLang();
+
+        })
+        $('#zipcode').keypress(function(e){
+            if(e.which == 13){
+                changezipcode();
+            }
+        });
+    });
 
     });
