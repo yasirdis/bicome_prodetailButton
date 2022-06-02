e80961f95171ebc7ab6a125e99b558c7217b29a7
yasirdis <yasir.dis@gmail.com>
2022-06-02 16:52:53 +0530

    jjjj

--- pd_detail.js
+++ pd_detail.js
@@ -1,3 +1,21 @@
-    {{#if page_type '==' 'default'}}
-   		 alert("sdcsdcdcsdcsdc");
-    {{/if}}
+window.onload=( function() {
+
+var widget = document.getElementsByClassName("card-figcaption-body");
+for(var i=0;i<widget.length;i++){
+    const el = widget[i].closest('.card-figure');
+
+var url = el.querySelectorAll('.card-figure__link')[0].href;
+
+    
+
+widget[i].innerHTML = widget[i].innerHTML + '<a class="button button--small card-figcaption-button " data-product-id="103" href='+url+'>Product detail</a>';
+
+    
+}
+    if(widget = document.getElementsByClassName("productView")[0]){
+        var widget = document.getElementsByClassName("productView-options");
+widget[0].innerHTML = widget[0].innerHTML + '<button type="button">BOPIS</button>';
+    }
+    
+
+    });
