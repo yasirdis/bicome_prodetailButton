486a64193fb1116354e95e750d9f5657eb3bdef6
yasirdis <yasir.dis@gmail.com>
2022-06-08 16:18:28 +0530

    Create theamobject.js

---
+++ theamobject.js
@@ -0,0 +1,17 @@
+import PageManager from './page-manager';
+
+export default class Product extends PageManager {
+    constructor(context) {
+        super(context);
+        this.url = window.location.href;
+        this.$reviewLink = $('[data-reveal-id="modal-review-form"]');
+        this.$bulkPricingLink = $('[data-reveal-id="modal-bulk-pricing"]');
+    }
+
+    onReady() {
+      // ...
+      // Example Code
+      alert("dsdsd");
+    }
+    // ...
+}
