9e07769d75d7d96c2d05762a7b26419f6b51b039
yasirdis <yasir.dis@gmail.com>
2022-06-09 11:12:23 +0530

    Update pd_detail.js

--- pd_detail.js
+++ pd_detail.js
@@ -1,25 +1,12 @@
 var widget = document.getElementsByClassName("card-figcaption-body");
-   function on() {
-  document.getElementById("overlay").style.display = "block";
-      addtocartpickup("openModal");
-}
-
-function off() {
-  document.getElementById("overlay").style.display = "none";
-}
 for(var i=0;i<widget.length;i++){
     const el = widget[i].closest('.card-figure');
-
 var url = el.querySelectorAll('.card-figure__link')[0].href;
-
     
-
 widget[i].innerHTML = widget[i].innerHTML + '<a class="button button--small card-figcaption-button " data-product-id="103" href='+url+'>Product detail</a>';
-
     
 }
             if(document.getElementsByClassName("productView")[0]){
-               
         var widget = document.getElementsByClassName("productView-options");
 widget[0].innerHTML = widget[0].innerHTML+
 '<input type="hidden" id="config-searchterm" name="config-searchterm" value="" />'+
@@ -36,32 +23,33 @@ widget[0].innerHTML = widget[0].innerHTML+
 '<input type="hidden" id="apibaseurl"  value="https://api.quiverstest.com/" />'+
 '<input type="hidden" id="sitebaseurl"  value="" />'+
 '<input type="hidden" id="custIp" name="custIp"  value="" />'+
-'<input type="hidden" id="searchterm" name="searchterm" value="885256" />'+
+'<input type="hidden" id="searchterm" name="searchterm" value="" />'+
  '   <input type="hidden" id="store" name="store"   value="" />'+
 '<input type="hidden" id="modalopen"   value="openModal" />'+
 '<input type="hidden" id="modalclose"    value="closeModal" />'+
-   '<div id="overlay" onclick="off()">'
-               '<div id="store-modal" style="display:none;">'+
+   '<div id="quiversmodalappend"  class="quiversaction" >'+
+                    '<button type="button"'+ 
+                            'title="Pick Up"'+
+                            'class="action primary quiverspickitup button button--primary"'+
+                            'onclick="addtocartpickup(\'openModal\');"'+
+                            'id="product-pickitup-button" >'+
+                        '<span>Pick up</span>'+
+                    '</button>'+
+        '</div>'+
+                '<div id="store-modal" style="display:none;">'+
     '<div class="quiverswrapper">'+
-        '<label class="labelzipcode">Showing location near zipcode:</label>'+
-        '<input type="text" class="zipcode" id="zipcode" value="" >'+
-        '<button type="button" onclick="changezipcode()"><i class="fa fa-search"></i></button>'+
-        '<input type="hidden" class="edit" id="edit" value="">'+
+ '       <label class="labelzipcode">Showing location near zipcode:</label>'+
+  '      <input type="text" class="zipcode" id="zipcode" value="" >'+
+   '     <button type="button" onclick="changezipcode()"><i class="fa fa-search"></i></button>'+
+      '  <input type="hidden" class="edit" id="edit" value="">'+
     '</div>'+
     '<div class="popup-body" id="popup-body">'+
     '<div class="alert alert-danger fade in alert-dismissible">'+
     'Could not find stores for this product near your location. Please change the zipcode and try again.'+
     '</div>'+
     '</div>'+
-'</div>'+
-               '</div>'+
-'<div style="padding:20px">'+
-  '<button onclick="on()">Pick up</button>'+
 '</div>';
-                
+
 getLatLang();
-                    
+                                
     }
-
-
-    
